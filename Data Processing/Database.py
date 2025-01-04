a31cf4782f6730ecb93a12e5728574722e1df843
CodeWizardMerlin <zklossner@gmail.com>
2025-01-04 18:40:25 -0500

    added more functionality to the database

---
+++ Data Processing/Database.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+class Database:
+    def __init__(self, db_name='data.db'):
+        self.connection = sqlite3.connect(db_name)
+        self.cursor = self.connection.cursor()
+        self.create_table()
+
+    def create_table(self):
+        table_create = "CREATE TABLE IF NOT EXISTS crash_data(crash_point REAL)"
+        self.cursor.execute(table_create)
+        self.connection.commit()
+
+    def print_table(self):
+        print(self.cursor.execute("SELECT * FROM crash_data").fetchall())
+
+    def insert_data(self, crash_point: float):
+        self.cursor.execute("INSERT INTO crash_data (crash_point) VALUES (?)", (crash_point,))
+        self.connection.commit()
+
+    def clear_table(self):
+        self.cursor.execute("DELETE FROM crash_data")
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
+
+    def process_file(self):
+        with open("Raw data.txt") as file:
+            raw_data = file.read()
+            data_values = raw_data.split()
+            for value in data_values:
+                crash_point = float(value)
+                self.insert_data(crash_point)
---
+++ Data Processing/Process and Optimization.py
@@ -0,0 +1,8 @@
+from Database import Database
+
+db = Database()
+
+db.process_file()
+db.print_table()
+db.clear_table()
+db.close()
