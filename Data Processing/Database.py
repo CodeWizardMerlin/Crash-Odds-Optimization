25be1e803aa0f9ed1ca280191e4aa3f7754ad4c5
CodeWizardMerlin <zklossner@gmail.com>
2025-01-05 22:48:00 -0500

    first itteration of the data processing code

--- Data Processing/Database.py
+++ Data Processing/Database.py
@@ -14,6 +14,11 @@ class Database:
     def print_table(self):
         print(self.cursor.execute("SELECT * FROM crash_data").fetchall())
 
+    def get_all_data(self):
+        self.cursor.execute("SELECT crash_point FROM crash_data")
+        data = self.cursor.fetchall()
+        return [row[0] for row in data]
+
     def insert_data(self, crash_point: float):
         self.cursor.execute("INSERT INTO crash_data (crash_point) VALUES (?)", (crash_point,))
         self.connection.commit()
@@ -22,9 +27,6 @@ class Database:
         self.cursor.execute("DELETE FROM crash_data")
         self.connection.commit()
 
-    def close(self):
-        self.connection.close()
-
     def process_file(self):
         with open("Raw data.txt") as file:
             raw_data = file.read()
@@ -32,3 +34,10 @@ class Database:
             for value in data_values:
                 crash_point = float(value)
                 self.insert_data(crash_point)
+
+    def table_size(self):
+        size = self.cursor.execute("SELECT COUNT(*) FROM crash_data").fetchone()[0]
+        return size
+    
+    def close(self):
+        self.connection.close()
--- Data Processing/Process and Optimization.py
+++ Data Processing/Process and Optimization.py
@@ -1,8 +1,33 @@
 from Database import Database
 
-db = Database()
+#first array of every element with repeats
+#second array of every element with no repeats from the dataset
+#third array with the same size as the first to put results in
 
+#use threads to process adding every data point in the data table (threads after I get it working with no threads)
+
+#run each data from first array though the entire second array and += if the data number is less than or equal to the second array num
+#add result number to the third array withth e same index as the first array number that was used
+#get the index of the largest number in the third array and print the number from the first array with the same index
+
+db = Database()
 db.process_file()
-db.print_table()
-db.clear_table()
-db.close()
+
+all_datapoints = db.get_all_data()
+points_to_check = list(set(all_datapoints))
+processed_points = [0] * len(all_datapoints)
+
+def process_data(point, index):
+    for i in range(len(all_datapoints)):
+        if all_datapoints[i] <= point:
+            processed_points[index] += all_datapoints[i]
+
+for i in points_to_check:
+    process_data(i, points_to_check.index(i))
+
+highest_index = 0
+for i in processed_points:
+    if i > highest_index:
+        highest_index = processed_points.index(i)
+
+print(points_to_check[highest_index])
