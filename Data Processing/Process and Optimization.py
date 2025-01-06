3d7f84b7e6a703a1bf3e5eed0fde63c8df7fdb12
CodeWizardMerlin <zklossner@gmail.com>
2025-01-06 18:08:03 -0500

    fixed the wrong optimal number being displayed

--- Data Processing/Process and Optimization.py
+++ Data Processing/Process and Optimization.py
@@ -7,6 +7,7 @@ all_datapoints = db.get_all_data()
 points_to_check = sorted(list(set(all_datapoints)))
 processed_points = [0] * len(points_to_check)
 
+#could optimise to be faster with threads
 def process_data(index, point):
     for i in all_datapoints:
         if point <= i:
@@ -16,15 +17,17 @@ for index, point in enumerate(points_to_check):
     process_data(index, point)
 
 highest_index = 0
-for i in processed_points:
-    if i > highest_index:
-        highest_index = processed_points.index(i)
+highest_value = 0
+for index, value in enumerate(processed_points):
+    if value > highest_value:
+        highest_index = index
+        highest_value = value
 
 print("optimal number: ", points_to_check[highest_index])
-print("index: ", highest_index)
+print("highest value: ", processed_points[highest_index])
 
 plt.plot(points_to_check, processed_points)
-plt.xlabel('Data Points')
+plt.xlabel('Return based on chosen multiplier')
 plt.ylabel('Processed Points')
 plt.title('Processed Points Line Graph')
 plt.show()
