ce3e52eb9d051a115cb21d897735eae490f38fd2
CodeWizardMerlin <zklossner@gmail.com>
2025-01-14 01:07:00 -0500

    Start of scraping unsorted data

---
+++ Data Writer/LocalStorageEditor.py
@@ -0,0 +1,10 @@
+class LocalStorage:
+
+    def __init__(self, driver) :
+        self.driver = driver
+
+    def get(self, key):
+        return self.driver.execute_script("return window.localStorage.getItem(arguments[0]);", key)
+
+    def set(self, key, value):
+        self.driver.execute_script("window.localStorage.setItem(arguments[0], arguments[1]);", key, value)
--- Data Writer/Webscrape.py
+++ Data Writer/Webscrape.py
@@ -1,17 +1,21 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
-from LocalStorage import LocalStorage
+from selenium.webdriver.common.by import By
+from LocalStorageEditor import LocalStorage
 import requests
+from time import sleep
 
 driver = webdriver.Firefox()
 driver.get("https://pg-stage.rpd.cloud/?partnerId=4&currency=USD&lan=en&gameId=34&mode=fun")
 
+# when opened, the crash website has a confirm button that needs to be bypassed to read the data from the html
+# the website usues local storage to skip the popup when reloading the page, this code edits the local storage to bypass the button
 storage = LocalStorage(driver)
-string = storage.get("crash_34_userSettings")
-string2 = string.replace("null", "263")
-print(string2)
-storage.set("crash_34_userSettings", string2)
-print(storage.get("crash_34_userSettings"))
-
+initial_storage = storage.get("crash_34_userSettings")
+modified_storage = initial_storage.replace("null", "263") # 263 is the value normally assigned after clicking continue
+storage.set("crash_34_userSettings", modified_storage)
 driver.refresh()
+
+sleep(3)
+driver.switch_to.frame(driver.find_element(By.CSS_SELECTOR, "#game-frame"))
+messy_data = driver.find_element(By.CLASS_NAME, "scrollable-container").text
